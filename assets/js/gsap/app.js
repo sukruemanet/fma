0baa20c130b9dbe693496257987b1e3dbab467ac
SukruEmanet <sukruemanet@gmail.com>
2022-12-02 00:25:23 +0300

    fix

--- assets/js/gsap/app.js
+++ assets/js/gsap/app.js
@@ -13,13 +13,14 @@ gsap.registerPlugin(ScrollTrigger);
 // });
 
 //Pin Single Content
-let st = ScrollTrigger.create({
-  trigger: '.start',
-  pin: true,
-  start: "top top",
-  end: "+=100%",
-  pinSpacing: false,
-});
+// let st = ScrollTrigger.create({
+//   trigger: ".start",
+//   pin: true,
+//   start: "top top",
+//   end: "bottom top",
+//   pinSpacing: false,
+//   markers: true
+// });
 
 const scrollAnimated = gsap.utils.toArray('.start-content.bodyScroll');
 scrollAnimated.forEach((box, i) => {
@@ -35,45 +36,31 @@ scrollAnimated.forEach((box, i) => {
         // end: "+=450%",
         start: "center 45%",
         // end: "center center",
-        id: "scrubbbbbb",
+        id: "scale",
     });
 });
 
-const bodybg = gsap.utils.toArray('.start');
-bodybg.forEach((bodybg, i) => {
-    const anim = gsap.fromTo(bodybg, { backgroundColor:"#fff" }, { backgroundColor:"#14141D" });
-    ScrollTrigger.create({
-        trigger: bodybg,
-        animation: anim,
-        toggleActions: 'restart none none none',
-        scrub:true,
-        // markers:true,
-        end: "+=800%",
-        // end: "+=450%",
-        start: "center center",
-        // end: "center center",
-        id: "start",
+// const bodybg = gsap.utils.toArray('.start-bg');
+// bodybg.forEach((bodybg, i) => {
+//     const anim = gsap.fromTo(bodybg, { backgroundColor:"#fff" }, { backgroundColor:"#14141D" });
+//     ScrollTrigger.create({
+//         trigger: bodybg,
+//         animation: anim,
+//         toggleActions: 'restart none none none',
+//         scrub:true,
+//         markers:true,
+//         end: "+=800%",
+//         // end: "+=450%",
+//         start: "center center",
+//         end: "center center",
+//         id: "start-bg",
 
-    });
-});
+//     });
+// });
 
-gsap.to(".arrowdown", { y: -25, ease: "power1.inOut", repeat: -1, duration:1.2, yoyo: true, delay:3,});
-
-//Fadeup
-const fadeupAnimated = gsap.utils.toArray('.fadeup');
-fadeupAnimated.forEach((box, i) => {
-  const anim = gsap.fromTo(box, { autoAlpha: 0, y: 150 }, { duration: 1, autoAlpha: 1, y: 0 });
-  ScrollTrigger.create({
-    trigger: box,
-    animation: anim,
-    toggleActions: 'play none none none',
-    once: true
-  });
-});
-
-//FadeIn Text
-const fadeInAnimated = gsap.utils.toArray('.imageScale');
-fadeInAnimated.forEach((box, i) => {
+//Image Scale
+const imageScale = gsap.utils.toArray('.imageScale');
+imageScale.forEach((box, i) => {
   const anim = gsap.fromTo(box, { scale: 1.8,willChange: "transform",}, { duration: 4, autoAlpha: 1, scale: 1, willChange: "transform",});
   ScrollTrigger.create({
     trigger: box,
@@ -81,26 +68,26 @@ fadeInAnimated.forEach((box, i) => {
     toggleActions: 'play none none none',
     once: true
   });
-
-
-
-const mm = gsap.matchMedia();
-  mm.add("(min-width: 768px)", () => {
-
-      const startcard = gsap.utils.toArray('.card-items .caption .head');
-      startcard.forEach((startcard, i) => {
-        const anim = gsap.fromTo(startcard, { scale: 1.2, willChange: "transform", }, {scale: 1, duration:10, willChange: "transform", });
-        ScrollTrigger.create({
-          trigger: startcard,
-          animation: anim,
-          toggleActions: 'play none none none',
-          scrub: 1,
-          start: '-20% center',
-          end: '-40% top',
-        });
-      });
-
-    });
 });
 
+const timeline = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".start-content",
+    toggleActions: "play none none none",
+    // markers: true,
+    scrub: true,
+    pin: ".start",
+    pinSpacing: false,
+    start: "top+=1 top",
+    end: "bottom top",
+    // end: "bottom top+=10%",
+    id: "start-bg"
+  }
+});
+
+timeline.to(
+  ".start-bg",{
+    backgroundColor: "#CECFD4",
+  }
+);
 
